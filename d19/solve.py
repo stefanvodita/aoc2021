3965e525d7b970c5d00473c6b4aa37430a7d25ad
The-Dunadan <stefan.vodita@gmail.com>
2021-12-22 00:54:09 +0200

    Solve day 19

--- d19/solve.py
+++ d19/solve.py
@@ -5,6 +5,7 @@ import sys
 from collections import Counter
 from copy import deepcopy
 from functools import reduce
+from itertools import combinations, product
 from math import ceil, floor
 from operator import mul
 from queue import PriorityQueue
@@ -191,8 +192,78 @@ def solve1(filename):
     count = len(beacons)
     print(count)
 
+def intersect2(scanners, a, b, scanpos):
+    pa = scanners[a]
+    pb = scanners[b]
+
+    mem = {}
+    pts = {}
+
+    for p1 in pa:
+        for p2 in pb:
+            for k, rot in enumerate(ROTS):
+                p2m = apply_rot(p2, rot)
+                dist = p1 - p2m
+                #if comp_max_dist(dist) > 2 * LOS:
+                #    continue
+
+                memkey = (k, tuple(dist))
+                mem[memkey] = mem.get(memkey, 0) + 1
+                pts[memkey] = pts.get(memkey, []) + [(deepcopy(p1), deepcopy(p2))]
+                if mem[memkey] >= MATCHES:
+                    # change axis
+                    change_axis(pb, rot, dist)
+                    # change scanpos
+                    #print(a, b)
+                    # scanpos[b] = scanpos[a] + dist
+                    scanpos[b] = p1 - apply_rot(pts[memkey][-1][1], rot)
+                    #print(scanpos[a])
+                    #print(scanpos[b])
+                    
+                    #tmp = sorted(mem, key=lambda x: mem[x], reverse=True)
+                    #print(str(tmp[0]) + ": " + str(mem[tmp[0]]))
+                    #print(str(tmp[1]) + ": " + str(mem[tmp[1]]))
+                    #print(str(tmp[2]) + ": " + str(mem[tmp[2]]))
+                    #print(pts[memkey])
+                    return True
+    return False
+
+def get_md(p1, p2):
+    return sum(abs(p1 - p2))
+
 def solve2(filename):
-    pass
+    global ROTS
+    ROTS = np.array(list(map(np.array, ROTS)))
+
+    scanners = get_input(filename)
+    scanners = np.array(scanners)
+    
+    scanpos = np.array([[0, 0, 0] for _ in scanners])
+    
+    known = [0]
+    i = 0
+    while True:
+        base = known[i]
+        print("base is " + str(base))
+        for j in range(len(scanners)):
+            if j in known:
+                continue
+            if intersect2(scanners, base, j, scanpos):
+                known.append(j)
+                print(str(base) + " matches " + str(j))
+        i += 1
+        if i == len(known):
+            break
+    print(known)
+    print(scanpos)
+    print()
+    
+    hmd = 0
+    for scanpos1, scanpos2 in combinations(scanpos, r=2):
+        md = get_md(scanpos1, scanpos2)
+        if md > hmd:
+            hmd = md
+    print(hmd)
 
 if __name__ == "__main__":
     if sys.argv[1] == "1":
